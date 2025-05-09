9add45cd08807e0ccd60cbb1bc452e61e6a39d3e
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2025-05-09 21:54:12 +0300

    feat: Initial setup for hangman-game

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import HangMan from "./pages/index.jsx";
+export {HangMan}
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+
+const HangMan = () => {
+  return (
+    <Box>test</Box>
+      
+  )
+}
+
+export default HangMan
